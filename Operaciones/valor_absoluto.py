91d49d4c0d6afb2301e125b3e8665c2f77de35a6
pipQUasar <mftoba963@gmail.com>
2024-05-24 20:59:20 -0300

    Nuevos cambios

--- Operaciones/valor_absoluto.py
+++ Operaciones/valor_absoluto.py
@@ -1,22 +1,25 @@
 def valor_absoluto(numero):
-      """
-      Se calculará el valor absoluto |numero| utilizando la funcion abs().
+  """
+  Se calculará el valor absoluto |numero| utilizando la funcion abs().
       
-      Args:
-          numero (int, float): Este será el numero ingresado por el usuario
+  Args:
+    numero (int, float): Este será el numero ingresado por el usuario
 
-      Raises:
-          ValueError: "Error! Se debe proporcionar al menos un numero..."
+  Raises:
+    ValueError: "Error! Se debe proporcionar al menos un numero..."
 
-      Returns:
-          int/float: El resultado absoluto del valor ingresado por el usuario
-      """
-      if not numero:    # Si no se ingresa un número, no se puede operar nada.
-        raise ValueError("Error! Se debe proporcionar al menos un numero...")
+  Returns:
+    int/float: El resultado absoluto del valor ingresado por el usuario
+  """
+  if not numero:    # Si no se ingresa un número, no se puede operar nada.
+    raise ValueError("Error! Se debe proporcionar al menos un numero...")
   
-      resultado = abs(numero)
-      return resultado
+  resultado = abs(numero)
+  return resultado
 
 # Ejemplo de uso
-resultado = valor_absoluto(-8)
-print(resultado) # REsultado esperado: 8
+try:
+    resultado = valor_absoluto(-8)
+    print(resultado) # Resultado esperado: 8
+except ValueError as err:
+      print(err)    
